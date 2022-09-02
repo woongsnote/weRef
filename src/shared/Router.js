74f274713ae33bff3c74c78f8733f8197dfa8906
woongsnote <mjw92@kakao.com>
2022-09-02 18:43:13 +0900

    디테일 페이지 init

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -2,16 +2,18 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../components/home/Home";
 import AddPost from "../components/addPost/AddPost";
+import Detail from "../components/detail/Detail";
 
 const Router = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                {/* 각자 작업한 router 설정 */}
-                <Route path="/" element={<Home />} />
-                <Route path="/addPost" element={<AddPost />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* 각자 작업한 router 설정 */}
+        <Route path="/" element={<Home />} />
+        <Route path="/addPost" element={<AddPost />} />
+        <Route path="/detail" element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 export default Router;
