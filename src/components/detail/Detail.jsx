8ae405fe33ab583d408cf588a5973a3d3945fc28
woongsnote <mjw92@kakao.com>
2022-09-03 09:39:11 +0900

    ui update

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -1,42 +1,54 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+//import { useNavigate } from "react-router-dom";
 
 import { styled } from "@mui/material/styles";
 import { Box, Button, Link, Paper } from "@mui/material";
-import Image from "mui-image";
 import Grid from "@mui/material/Grid";
+import Image from "mui-image";
 
 import Header from "../header/Header";
+import { RESP } from "../../shared/response";
 
 export default function Detail() {
-  const navigate = useNavigate();
+  //데이터 가져오는 동안 로딩 처리
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState({});
 
-  const data = {
-    title: "안녕하세요",
-    imageUrl: "https://picsum.photos/id/176/2000",
-    link: "https://nomadcoders.co/",
-    description: "테스트중입니다",
-    createdAt: Date.now(),
-    modifiedAt: Date.now(),
-  };
-  console.log(data);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+      setData(RESP.DETAIL.result);
+    }, 1000);
+  }, []);
 
-  return (
+  // const navigate = useNavigate();
+
+  const links = data.urlLink;
+
+  // const EditButton = () => {
+  //   return (
+  //     <Box sx={{ display: "flex", alignItems: "flex-end" }} hidden>
+  //       {/* Edit page 이동(로그인 했을 때만 표시) */}
+  //       <Link
+  //         variant="body1"
+  //         onClick={() => {
+  //           navigate("/addPost");
+  //         }}
+  //         underline="none"
+  //         component={"button"}
+  //       >
+  //         수정하기
+  //       </Link>
+  //     </Box>
+  //   );
+  // };
+
+  return isLoading ? (
+    <Box>정보를 가져오는 중입니다.. </Box>
+  ) : (
     <>
       <Header />
-      <Box>
-        {/* Edit page 이동(로그인 했을 때만 표시) */}
-        <Link
-          variant="body2"
-          onClick={() => {
-            navigate("/addPost");
-          }}
-          underline="none"
-          component={"button"}
-        >
-          수정하기
-        </Link>
-      </Box>
+
       {/* 작성한 내용 표시 */}
       <Box sx={{ border: "1px solid #eee" }} m={2} p={3} borderRadius={2}>
         <Grid container spacing={2}>
@@ -49,23 +61,54 @@ export default function Detail() {
             <Item>
               <h2>{data.title}</h2>
               <p>{data.description}</p>
-              <p>{data.link}</p>
-              <p>{data.link}</p>
-              <p>{data.link}</p>
-              <p>{data.link}</p>
-              <p>{data.link}</p>
             </Item>
+            <Box>
+              <h3>Ref List</h3>
+              {/* 링크 리스트 */}
+              {links.map((link, index) => {
+                return (
+                  <LinkItem key={index}>
+                    <Link href={link}>{link}</Link>
+                  </LinkItem>
+                );
+              })}
+            </Box>
           </Grid>
         </Grid>
       </Box>
-      {/* 댓글 리스트``````` */}
-      <Box>
-        <Button>새 댓글</Button>
+      {/* 댓글 리스트 */}
+      <Box sx={{ border: "1px solid #eee" }} m={2} p={3} borderRadius={2}>
+        {/* 새 댓글 누르면 댓글 컴포넌트 생성 */}
+        <Button
+          onClick={() => {
+            console.log("댓글 추가");
+          }}
+        >
+          새 댓글
+        </Button>
         <Box>
           댓글
-          <Button>추가</Button>
-          <Button>삭제</Button>
-          <Button>편집</Button>
+          <Button
+            onClick={() => {
+              console.log("댓글 저장");
+            }}
+          >
+            추가
+          </Button>
+          <Button
+            onClick={() => {
+              console.log("댓글 저장");
+            }}
+          >
+            삭제
+          </Button>
+          <Button
+            onClick={() => {
+              console.log("댓글 저장");
+            }}
+          >
+            편집
+          </Button>
         </Box>
       </Box>
     </>
@@ -75,6 +118,15 @@ const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
   padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
+
+const LinkItem = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  margin: theme.spacing(2),
   textAlign: "center",
   color: theme.palette.text.secondary,
 }));
---
+++ src/shared/response.js
@@ -0,0 +1,39 @@
+export const RESP = {
+  DETAIL: {
+    ok: true,
+    result: {
+      title: "안녕하세요",
+      imageUrl:
+        "https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80",
+      urlLink: [
+        "https://nodejs.org/",
+        "https://www.naver.com",
+        "https://github.com/",
+        "https://www.google.com/",
+        "https://nomadcoders.co/",
+      ],
+      description: "테스트중입니다",
+    },
+  },
+  COMMENTS: {
+    ok: true,
+    result: [
+      {
+        writer: "tester1",
+        comment: "test중입니다.1",
+      },
+      {
+        writer: "tester2",
+        comment: "test중입니다.2",
+      },
+      {
+        writer: "tester3",
+        comment: "test중입니다.3",
+      },
+      {
+        writer: "tester4",
+        comment: "test중입니다.4",
+      },
+    ],
+  },
+};
