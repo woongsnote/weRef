26c6297357daf8c3630c6c2073c33132853dfa68
wildgallop21 <wildgallop21@gmail.com>
2022-09-08 13:59:45 +0900

    유효성검사 아이디 버튼누를때만 되도록

--- src/components/signup/SignUp.jsx
+++ src/components/signup/SignUp.jsx
@@ -14,74 +14,120 @@ import Box from "@mui/material/Box";
 // import styled from 'styled-components;'
 // import { StylesProvider } from '@material-ui/core/styles';
 import axios from "axios";
+import "./SignUp.css";
+import FormControl from '@mui/material/FormControl';
+
 
 export default function SignUp() {
   const dispatch = useDispatch();
   const usernameInput = useRef(null);
-  const nameInput = useRef(null);
   const passwordInput = useRef(null);
+  const passwordConfirmInput = useRef(null);
+  const { nanoid } = require('nanoid')
+  const [isUsername, setisUsername] = useState(false)
+  const [isPassword, setIsPassword] = useState(false)
+  const [isPasswordConfirm, setIsPasswordConfirm] = useState(false)
+  const [usernameMessage, setUsernameMessage] = useState('usernamemsgtest')
+  const [passwordMessage, setPasswordMessage] = useState('')
+  const [passwordConfirmMessage, setPasswordConfirmMessage] = useState('')
+
   // console.log(usernameInput)
   useEffect(() => {
     // dispatch(__deleteReviews())
     dispatch(createUserThunk());
-    }, [dispatch]);
+  }, [dispatch]);
+
+//   const usernameRegex =
+//   /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
+//   const userCurrent = usernameInput.current.value
+//   // const userCurrent = usernameInput
+//   console.log(userCurrent)
+// setisUsername(userCurrent)
+
+// if (!usernameRegex.test(userCurrent)) {
+//   setUsernameMessage('아이디 형식이 틀렸어요! 다시 확인해주세요 ㅜ ㅜ')
+// } else {
+//   setUsernameMessage('올바른 아이디 형식이에요 : )')
+//   setisUsername(true)
+// }
+
 
   const signupClickHandler = async () => {
-    // e.preventDefault();
     const newUser = {
-    //   id: nanoid(),
+      id: nanoid(),
       username: usernameInput.current.value,
-      nickname: nameInput.current.value,
+      // nickname: usernameInput.current.value,
       password: passwordInput.current.value,
-      passwordConfirm: passwordInput.current.value
-
+      passwordConfirm: passwordConfirmInput.current.value,
     };
-    axios({ url:"http://13.125.246.47:8080/api/member/signup", 
-    method:"POST",
-    data:newUser,
-    withCredentials:true})
+
+    const usernameRegex =
+    /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
+    const userCurrent = usernameInput.current.value
+    // const userCurrent = usernameInput
+    setisUsername(userCurrent)
+    console.log(isUsername)
+    console.log(userCurrent)
+    console.log(usernameInput)
+
+    if (!usernameRegex.test(userCurrent)) {
+  setUsernameMessage('아이디 형식이 틀렸어요! 다시 확인해주세요')
+} else {
+  setUsernameMessage('올바른 아이디 형식이에요 : )')
+  // setisUsername(true)
+}
+
+
+
+    axios({
+      id:nanoid(),
+      // url: "http://13.125.246.47:8080/api/member/signup",
+      url: "http://localhost:3001/users",
+      method: "POST",
+      data: newUser,
+      withCredentials: true,
+    });
     // dispatch(createUserThunk(newUser));
   };
 
   return (
     <div className="SignUp">
-      {/* <SignUpView /> */}
-      {/* <StylesProvider injectFirst>
-    <BoxContainer> */}
-
-    <div>
+      <div>
+        <Box
+        component="form"
+        sx={{
+          '& > :not(style)': { m: 2, width: '50ch' },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        {/* <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        <TextField id="filled-basic" label="Filled" variant="filled" /> */}
+        <TextField id="standard-basic" label="UserID" variant="standard" 
+        ref={usernameInput} type="text" />
+        <TextField id="standard-basic" label="Password" variant="standard" />
+        <TextField id="standard-basic" label="PasswordConfirm" variant="standard" />
+      </Box>
+      </div>
+      <div>
+        {/* username : <input  type="text" /> <br /> */}
         username : <input ref={usernameInput} type="text" /> <br />
-        nickname : <input ref={nameInput} type="text" /> <br />
-        password : <input ref={passwordInput} type="password" />
-    </div>
-    <div>
-        <TextField
-            error
-            id="filled-error"
-            label="Error"
-            defaultValue="username을 입력하세요"
-            variant="filled"
-        />
-        <TextField
-            error
-            id="filled-error-helper-text"
-            label="Error"
-            defaultValue="Hello World"
-            helperText="Incorrect entry."
-            variant="filled"
-        />
-    </div>
-    <Button variant="contained" onClick={signupClickHandler}>
+        <div>{usernameMessage}</div>
+        
+        password : <input ref={passwordInput} type="password" /> <br />
+        passwordConfirm : <input ref={passwordConfirmInput} type="password" />
+      </div>
+      <div>
+      </div>
+      <Button variant="contained" onClick={signupClickHandler}>
         회원가입
-    </Button>
+      </Button>
       {/* <button onClick={signupClickHandler}>회원가입</button> */}
-    {/* </BoxContainer>
+      {/* </BoxContainer>
     </StylesProvider> */}
     </div>
-
   );
 }
-
 
 // const BoxContainer = styled.div `
 //   padding: 6px 12px;
